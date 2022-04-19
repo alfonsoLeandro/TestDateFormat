efd7a227e5597b0edb4897788c8ef706518a69dc
Leandro Alfonso <leamanya24.6@gmail.com>
2022-04-19 19:36:13 -0300

    Initial commit

---
+++ DateFormat/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DateFormat
+{
+    public class Program
+    {
+        public static String ChangeFormat(String year)
+        {
+            return year.Substring(6) + "-" + year.Substring(3, 2) + "-" + year.Substring(0, 2);
+        }
+
+        public static void Main(string[] args)
+        {
+            string testDate = "10/11/1977";
+            Console.WriteLine("{0} se convierte a: {1} ", testDate, ChangeFormat(testDate));
+        }
+    }
+}
